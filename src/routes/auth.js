5c70dc8815838be1386496cc3a71424671278c1d
rogerwschmidt <rogerschmidt@gmail.com>
2018-04-18 21:32:19 -0700

    initial commit

---
+++ src/routes/auth.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const authController = require('../controllers/auth')
+
+//////////////////////////////////////////////////////////////////////////////
+// Basic CRUD Methods
+//////////////////////////////////////////////////////////////////////////////
+
+router.post('/login', authController.login)
+
+
+
+module.exports = router
