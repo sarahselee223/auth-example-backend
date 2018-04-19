1f99b35f8ad35c46931c9c757e1be95f5d474502
rogerwschmidt <rogerschmidt@gmail.com>
2018-04-19 07:17:10 -0700

    removed controller code for login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -16,26 +16,7 @@ const jwt = require('jsonwebtoken')
 //////////////////////////////////////////////////////////////////////////////
 
 function login(req, res, next){
-  // 1. Make sure that request is good
-  if(!req.body.username){
-    return next({ status: 400, message: 'Bad request'})
-  }
 
-  if(!req.body.password){
-    return next({ status: 400, message: 'Bad request'})
-  }
-
-  // 2. Attempt Login
-  authModel.login(req.body.username, req.body.password)
-  .then(function(user){
-
-    // 3. Create token
-    const token = jwt.sign({id: user.id}, process.env.SECRET)
-
-    // 4. Send back token
-    return res.status(200).send({ token })
-  })
-  .catch(next)
 }
 
 //////////////////////////////////////////////////////////////////////////////
