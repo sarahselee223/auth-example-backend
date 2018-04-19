fa3821b5dbc698dfc713c18ffe1a7e24ecfd1f9a
rogerwschmidt <rogerschmidt@gmail.com>
2018-04-19 07:29:19 -0700

    removed authorization

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -43,26 +43,11 @@ function login(req, res, next){
 //////////////////////////////////////////////////////////////////////////////
 
 function isAuthenticated(req, res, next){
-  const [scheme, credentials] = req.headers.authorization.split(' ')
 
-  jwt.verify(credentials, process.env.SECRET, (err, payload)=>{
-    if(err){
-      return next({ status: 401, message: 'Unauthorized' })
-    }
-
-    req.claim = payload
-
-    next()
-  })
 }
 
 function isSelf(req, res, next){
 
-  if(parseInt(req.params.userId) !== req.claim.id){
-    return next({ status: 401, message: 'Unauthorized' })
-  }
-
-  next()
 }
 
 
