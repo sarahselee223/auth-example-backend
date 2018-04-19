2589c1ea3b477f9258a0a30e536b0df23e930f47
rogerwschmidt <rogerschmidt@gmail.com>
2018-04-19 07:15:49 -0700

    removed login from the auth model

--- src/models/auth.js
+++ src/models/auth.js
@@ -18,30 +18,7 @@ const userModel = require('./users')
 //////////////////////////////////////////////////////////////////////////////
 
 function login(username, password){
-  let user
 
-  // 1. Check to see if user already exists
-  return userModel.getOneByUserName(username)
-  .then(function(data){
-    // 1a. if not, return a 400 with appropriate error message
-    if(!data) throw { status: 400, message: "Bad Request"}
-
-    // save user for later use
-    user = data
-
-    // 2. compare password in the database with the password provided by user
-    return bcrypt.compare(password, data.password)
-  })
-  .catch(bcrypt.MISMATCH_ERROR, function(){
-    // 3. If the passwords do not match, respond with 401 Unauthorized
-    throw { status: 401, message: "Unauthorized"}
-  })
-  .then(function(){
-    // 4. strip hashed password away from object
-    delete user.password
-    // 5. "return/continue" promise
-    return user
-  })
 }
 
 module.exports = {
