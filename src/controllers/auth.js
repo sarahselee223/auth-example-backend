99d59b202574815e8b14a0725e00944d3f8cb4c7
Sarah Lee <sarahselee223@gmail.com>
2018-11-26 13:31:10 -0800

    b

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -15,9 +15,30 @@ const jwt = require('jsonwebtoken')
 // 4. Send back token
 //////////////////////////////////////////////////////////////////////////////
 
-function login(req, res, next){
 
+function login(req, res, next){
+  // 1. Make sure that request is good
+  if(!req.body.username){
+    return next({ status: 400, message: 'Bad request'})
+  }
+
+  if(!req.body.password){
+    return next({ status: 400, message: 'Bad request'})
+  }
+
+  // 2. Attempt Login
+  authModel.login(req.body.username, req.body.password)
+  .then(function(user){
+
+    // 3. Create token
+    const token = jwt.sign({id: user.id}, process.env.SECRET)
+
+    // 4. Send back token
+    return res.status(200).send({ token })
+  })
+  .catch(next)
 }
+
 
 //////////////////////////////////////////////////////////////////////////////
 // Quality of Life functions
--- src/models/users.js
+++ src/models/users.js
@@ -1,5 +1,5 @@
 const db = require('../../db')
-const bcrypt = require('bcrypt-as-promised')
+const bcrypt = require('bcrypt')
 
 //////////////////////////////////////////////////////////////////////////////
 // Basic CRUD Methods
