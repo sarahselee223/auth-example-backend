31da7f1154bc4f7ccbcda1b4590d0d7bbcd69956
Sarah Lee <sarahselee223@gmail.com>
2018-11-26 12:59:39 -0800

    a

--- src/models/auth.js
+++ src/models/auth.js
@@ -18,7 +18,18 @@ const userModel = require('./users')
 //////////////////////////////////////////////////////////////////////////////
 
 function login(username, password){
-
+  let user
+  return userModel.getOneByUserName(username)
+  .then(function(data){
+    if(!data) throw {status: 400, message: "Bad request"}
+    user = data
+    return bcrypt.compare(password, data.password)
+  })
+  .then(function(status){
+    if(!status) throw {status: 401, message: "Unauthorized"}
+    delete user.password
+    return user
+  })
 }
 
 module.exports = {
